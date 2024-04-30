902cc2e29d08e30af7c62d09265a58c2dbc0d6ea
victorchiaka <chiakavictor205@gmail.com>
2024-04-30 19:04:40 +0100

    ft: added telescope and gruvbox colorscheme

--- init.lua
+++ init.lua
@@ -1,17 +1,2 @@
 require("core.keymaps")
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup(plugins, opts)
-
+require("core.plugins")
---
+++ lua/core/plugins.lua
@@ -0,0 +1,35 @@
+local cmd = vim.cmd
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local keymap = vim.keymap
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+     dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+local builtin = require("telescope.builtin")
+
+cmd([[colorscheme gruvbox]])
+
+return builtin -- This returns should happen at the end of the file
