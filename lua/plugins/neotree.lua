523d9d440e1e3ecc7970d6689abdb4767f00fb64
victorchiaka <chiakavictor205@gmail.com>
2024-05-08 10:09:21 +0100

    format: formated

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,9 +1,9 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", 
-    "MunifTanjim/nui.nvim",
-  }
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
 }
