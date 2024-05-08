51ba811e54e9ce201d4f073bec5391ce05892f4f
victorchiaka <chiakavictor205@gmail.com>
2024-05-08 15:49:14 +0100

    format: formated

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -1,52 +1,51 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-path",
-		"L3MON4D3/LuaSnip", -- snippet engine
-		"saadparwaiz1/cmp_luasnip", -- for autocompletion
-		"rafamadriz/friendly-snippets",
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "L3MON4D3/LuaSnip", -- snippet engine
+    "saadparwaiz1/cmp_luasnip", -- for autocompletion
+    "rafamadriz/friendly-snippets",
+  },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    -- loads vscode style snippets
+    require("luasnip.loaders.from_vscode").lazy_load()
+      cmp.setup({
+        completion = {
+	  completeopt = "menu,menuone,preview,noselect",
 	},
-	config = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
+        snippet = {
+	  expand = function(args)
+	    luasnip.lsp_expand(args.body)
+	  end,
+	},
+	window = {
+	  completion = cmp.config.window.bordered(),
+	  documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+	  ["<C-k>"] = cmp.mapping.select_prev_item(), -- select previous suggestion
+	  ["<C-j>"] = cmp.mapping.select_next_item(), -- select next suggestion
 
-		-- loads vscode style snippets
-		require("luasnip.loaders.from_vscode").lazy_load()
+	  ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+	  ["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-		cmp.setup({
-			completion = {
-				completeopt = "menu,menuone,preview,noselect",
-			},
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			window = {
-				completion = cmp.config.window.bordered(),
-				documentation = cmp.config.window.bordered(),
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-k>"] = cmp.mapping.select_prev_item(), -- select previous suggestion
-				["<C-j>"] = cmp.mapping.select_prev_item(), -- select next suggestion
+	  ["<C-Space>"] = cmp.mapping.complete(), -- Show completion window
+	  ["<C-e>"] = cmp.mapping.abort(), -- Close completion window
+	  ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
 
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-				["<C-Space>"] = cmp.mapping.complete(), -- Show completion window
-				["<C-e>"] = cmp.mapping.abort(), -- Close completion window
-				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-			}),
-
-			-- Auto completion source
-			sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-				{ name = "luasnip" },
-				{ name = "buffer" },
-				{ name = "path" },
-			}),
-		})
-	end,
+	-- Auto completion source
+	sources = cmp.config.sources({
+	  { name = "nvim_lsp" },
+	  { name = "luasnip" },
+	  { name = "buffer" },
+	  { name = "path" },
+	}),
+      })
+   end,
 }
