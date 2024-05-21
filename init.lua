77e7a73f4159d8e4a626b1bac4445cfd7533cb3d
victorchiaka <chiakavictor205@gmail.com>
2024-05-21 22:38:08 +0100

    added tailwind colorizer

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 require("keymaps")
 require("plugins_spec")
+
+vim.opt.clipboard:append('unnamedplus')
+
--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -1,51 +1,58 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip", -- snippet engine
-    "saadparwaiz1/cmp_luasnip", -- for autocompletion
-    "rafamadriz/friendly-snippets",
-  },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-
-    -- loads vscode style snippets
-    require("luasnip.loaders.from_vscode").lazy_load()
-      cmp.setup({
-        completion = {
-	  completeopt = "menu,menuone,preview,noselect",
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"L3MON4D3/LuaSnip", -- snippet engine
+		"saadparwaiz1/cmp_luasnip", -- for autocompletion
+		"rafamadriz/friendly-snippets",
+		{ "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
 	},
-        snippet = {
-	  expand = function(args)
-	    luasnip.lsp_expand(args.body)
-	  end,
-	},
-	window = {
-	  completion = cmp.config.window.bordered(),
-	  documentation = cmp.config.window.bordered(),
-	},
-	mapping = cmp.mapping.preset.insert({
-	  ["<C-k>"] = cmp.mapping.select_prev_item(), -- select previous suggestion
-	  ["<C-j>"] = cmp.mapping.select_next_item(), -- select next suggestion
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
 
-	  ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-	  ["<C-f>"] = cmp.mapping.scroll_docs(4),
+		-- loads vscode style snippets
+		require("luasnip.loaders.from_vscode").lazy_load()
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
+			formatting = {
+				format = function(entry, item)
+					return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+				end,
+			},
 
-	  ["<C-Space>"] = cmp.mapping.complete(), -- Show completion window
-	  ["<C-e>"] = cmp.mapping.abort(), -- Close completion window
-	  ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-k>"] = cmp.mapping.select_prev_item(), -- select previous suggestion
+				["<C-j>"] = cmp.mapping.select_next_item(), -- select next suggestion
 
-	-- Auto completion source
-	sources = cmp.config.sources({
-	  { name = "nvim_lsp" },
-	  { name = "luasnip" },
-	  { name = "buffer" },
-	  { name = "path" },
-	}),
-      })
-   end,
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+
+				["<C-Space>"] = cmp.mapping.complete(), -- Show completion window
+				["<C-e>"] = cmp.mapping.abort(), -- Close completion window
+				["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+			}),
+
+			-- Auto completion source
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
 }
