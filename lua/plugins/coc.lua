388f05762bea97c07f34a0440511629d976b8cde
victorchiaka <chiakavictor205@gmail.com>
2024-05-31 02:15:31 +0100

    change: changed the completion list next and prev key

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -30,20 +30,19 @@ M.config = function()
 	-- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
 	-- other plugins before putting this into your config
 	local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
-	keyset(
-		"i",
-		"<TAB>",
-		'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()',
+	keyset("i", "<c-j>",	'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()',
 		opts
 	)
-	keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
+
+	keyset("i", "<C-k>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 
 	-- Make <CR> to accept selected completion item or notify coc.nvim to format
 	-- <C-g>u breaks current undo, please make your own choice
 	keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
+
 	-- Use <c-j> to trigger snippets
-	keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
+	-- keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
 	-- Use <c-space> to trigger completion
 	keyset("i", "<c-space>", "coc#refresh()", { silent = true, expr = true })
 
