ec0fa6b92b28fa0680ff851165488a54378bfd69
victorchiaka <chiakavictor205@gmail.com>
2024-07-23 01:25:23 +0100

    added ts-rainbow

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,55 +1,63 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			ensure_installed = {
-				"c",
-				"lua",
-				"html",
-				"javascript",
-				"python",
-				"typescript",
-				"java",
-				"vue",
-				"css",
-				"scss",
-				"tsx",
-				"json",
-				"markdown",
-				"markdown_inline",
-				"bash",
-				"vim",
-				"cpp",
-				"dart",
-				"prisma"
-			},
-			filetypes = {
-				"html",
-				"javascript",
-				"typescript",
-				"javascriptreact",
-				"typescriptreact",
-				"svelte",
-				"vue",
-				"tsx",
-				"jsx",
-				"rescript",
-				"xml",
-				"php",
-				"markdown",
-				"astro",
-				"glimmer",
-				"handlebars",
-				"hbs",
-				"dart",
-				"prisma"
-			},
-			-- auto_install = true, -- To auto install parser for any language
-			sync_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local config = require("nvim-treesitter.configs")
+    config.setup({
+      ensure_installed = {
+        "c",
+        "lua",
+        "html",
+        "javascript",
+        "python",
+        "typescript",
+        "java",
+        "vue",
+        "css",
+        "scss",
+        "tsx",
+        "json",
+        "markdown",
+        "markdown_inline",
+        "bash",
+        "vim",
+        "cpp",
+        "dart",
+        "prisma",
+      },
+      filetypes = {
+        "html",
+        "javascript",
+        "typescript",
+        "javascriptreact",
+        "typescriptreact",
+        "svelte",
+        "vue",
+        "tsx",
+        "jsx",
+        "rescript",
+        "xml",
+        "php",
+        "markdown",
+        "astro",
+        "glimmer",
+        "handlebars",
+        "hbs",
+        "dart",
+        "prisma",
+      },
+      rainbow = {
+        enable = true,
+        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+        max_file_lines = nil, -- Do not enable for files with more than n lines, int
+        -- colors = {}, -- table of hex strings
+        -- termcolors = {} -- table of colour name strings
+      },
+      -- auto_install = true, -- To auto install parser for any language
+      sync_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
