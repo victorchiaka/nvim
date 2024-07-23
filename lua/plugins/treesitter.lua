03c4d2ca23f03c7ec22f54e68a5cd0defa416336
victorchiaka <chiakavictor205@gmail.com>
2024-07-23 20:22:41 +0100

    removed rainbow

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -46,14 +46,7 @@ return {
         "dart",
         "prisma",
       },
-      rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-      },
+
       -- auto_install = true, -- To auto install parser for any language
       sync_install = true,
       highlight = { enable = true },
