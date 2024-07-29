d0958b504663e4529ee9db3ed53478301511d46d
victorchiaka <chiakavictor205@gmail.com>
2024-07-29 10:14:04 +0100

    lil change

--- lua/plugins/nvim_highlight_colors.lua
+++ lua/plugins/nvim_highlight_colors.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     require("nvim-highlight-colors").setup({
       render = 'virtual',
+      -- virtual_symbol = '■',
       virtual_symbol = '●',
       virtual_symbol_prefix = '',
       virtual_symbol_suffix = ' ',
--- lua/plugins/solarized_osaka.lua
+++ lua/plugins/solarized_osaka.lua
@@ -6,7 +6,7 @@ return {
     priority = 1000,
     config = function()
       return {
-        transparent = true,
+        transparent = false,
       }
     end,
   },
