e4e7b802d1d25a2e574cb04346c5d7a852d5479e
Victor Chiaka <chiakavictor205@gmail.com>
2024-12-17 16:44:42 +0100

    .

--- lua/plugins/nvim_highlight_colors.lua
+++ lua/plugins/nvim_highlight_colors.lua
@@ -3,8 +3,8 @@ return {
   config = function()
     require("nvim-highlight-colors").setup({
       render = 'virtual',
-      -- virtual_symbol = '■',
-      virtual_symbol = '●',
+      virtual_symbol = '■',
+      -- virtual_symbol = '●',
       virtual_symbol_prefix = ' ',
       virtual_symbol_suffix = ' ',
       virtual_symbol_position = 'inline',
