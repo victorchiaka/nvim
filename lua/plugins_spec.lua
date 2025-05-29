ba524665e1e76fd9c4208bb45c032c9f8a825edd
victorchiaka <chiakavictor205@gmail.com>
2025-05-29 18:16:56 +0100

    .

--- lua/plugins_spec.lua
+++ lua/plugins_spec.lua
@@ -31,12 +31,12 @@ local builtin = require("telescope.builtin")
 -- cmd([[colorscheme gruvbox]]) -- to set gruvbox theme
 
 -- From this section to setup kanagawa theme
--- cmd("colorscheme kanagawa-lotus")
-cmd("colorscheme kanagawa-wave") -- default same as cmd("colorscheme kanagawa")
+-- cmd("colorscheme kanagawa-lotus") -- default same as cmd("colorscheme kanagawa")
+cmd("colorscheme kanagawa-wave")
 -- cmd("colorscheme kanagawa-dragon")
 
 -- From this section to setup one dark theme
-require("onedark").load()
+-- require("onedark").load()
 
 return {
   builtin = builtin,
