7740e9a9cd1d2ea1f3db6625473b8ff020515b8c
Victor Chiaka <victor.chiaka@originswift.dev>
2025-05-31 14:47:33 +0100

    .

--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -90,6 +90,7 @@ return {
       -- configure lua server (with special settings)
       lspconfig["lua_ls"].setup({
         capabilities = capabilities,
+        on_attach = on_attach,
         settings = {
           Lua = {
             -- make the language server recognize "vim" global
