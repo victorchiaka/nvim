b3bf581fbe13010dfc1803e90cf476ed145d1136
victorchiaka <chiakavictor205@gmail.com>
2025-06-21 19:00:51 +0100

    formatted configuration

--- lua/plugins/treesitter_textobject.lua
+++ lua/plugins/treesitter_textobject.lua
@@ -3,32 +3,36 @@ return {
   "nvim-treesitter/nvim-treesitter-textobjects",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
+    --[[  
+      Anything class represents types generally, maybe interface, structs, class and other types
+      Anything function represents routines generally
+    ]]    
     require("nvim-treesitter.configs").setup({
       textobjects = {
         select = {
           enable = true,
-          lookahead = true,   -- Automatically jump forward to textobj
+          lookahead = true, -- Automatically jump forward to textobj
           keymaps = {
             -- You can use the capture groups defined in textobjects.scm
             ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
+            ["if"] = "@function.inner",  -- Select inside a function
+            ["ac"] = "@class.outer",     -- Select outside class
             ["ic"] = "@class.inner",
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["ab"] = "@block.outer",
-            ["ib"] = "@block.inner",
+            ["aa"] = "@parameter.outer", -- Select outside parameter
+            ["ia"] = "@parameter.inner", -- Select inside parameter
+            ["ab"] = "@block.outer",     -- Selects the block and the block name (eg - function name and everything and end of function, class name and everything to end of class)
+            ["ib"] = "@block.inner",     -- Select inside a block
           },
           -- You can choose the select mode (default is charwise 'v')
           selection_modes = {
-            ['@parameter.outer'] = 'v',   -- charwise
-            ['@function.outer'] = 'V',    -- linewise
-            ['@class.outer'] = '<c-v>',   -- blockwise
+            ['@parameter.outer'] = 'v', -- charwise
+            ['@function.outer'] = 'V',  -- linewise
+            ['@class.outer'] = '<c-v>', -- blockwise
           },
         },
         move = {
           enable = true,
-          set_jumps = true,   -- whether to set jumps in the jumplist
+          set_jumps = true, -- whether to set jumps in the jumplist
           goto_next_start = {
             ["]m"] = "@function.outer",
             ["]]"] = "@class.outer",
