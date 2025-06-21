88713147704082d97d6c6875b86b26f918b39b34
victorchiaka <chiakavictor205@gmail.com>
2025-06-21 15:30:17 +0100

    ft: adding textobject

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
---
+++ lua/plugins/treesitter_textobject.lua
@@ -0,0 +1,52 @@
+-- Textobjects plugin (separate configuration)
+return {
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,   -- Automatically jump forward to textobj
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["ab"] = "@block.outer",
+            ["ib"] = "@block.inner",
+          },
+          -- You can choose the select mode (default is charwise 'v')
+          selection_modes = {
+            ['@parameter.outer'] = 'v',   -- charwise
+            ['@function.outer'] = 'V',    -- linewise
+            ['@class.outer'] = '<c-v>',   -- blockwise
+          },
+        },
+        move = {
+          enable = true,
+          set_jumps = true,   -- whether to set jumps in the jumplist
+          goto_next_start = {
+            ["]m"] = "@function.outer",
+            ["]]"] = "@class.outer",
+          },
+          goto_next_end = {
+            ["]M"] = "@function.outer",
+            ["]["] = "@class.outer",
+          },
+          goto_previous_start = {
+            ["[m"] = "@function.outer",
+            ["[["] = "@class.outer",
+          },
+          goto_previous_end = {
+            ["[M"] = "@function.outer",
+            ["[]"] = "@class.outer",
+          },
+        },
+      },
+    })
+  end,
+}
