2828fceaf727afd89b9754871cb219c6e630ed8c
victorchiaka <chiakavictor205@gmail.com>
2025-07-22 12:13:26 +0100

    ...

---
+++ lua/plugins/minty.lua
@@ -0,0 +1,4 @@
+return {
+  "nvzone/minty",
+  cmd = { "Shades", "Huefy" },
+}
---
+++ lua/plugins/volt.lua
@@ -0,0 +1,10 @@
+return {
+  "nvzone/volt",
+  lazy = true,
+  -- Uncomment one of these loading triggers based on your needs:
+  -- cmd = { "VoltCommand" }, -- if volt provides specific commands
+  -- event = "VeryLazy", -- load after Neovim startup is complete
+  -- keys = { -- if you want specific keybindings
+  --   { "<leader>v", "<cmd>VoltToggle<cr>", desc = "Toggle Volt" },
+  -- },
+}
