738de45757202ad61716e947ff43dcb3fcc12abd
victorchiaka <chiakavictor205@gmail.com>
2026-09-03 21:02:28 +0100

    ft: added good treesitter logic

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,7 +3,15 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         require("lualine").setup({
-            theme = "auto"
+            theme = "auto",
+            sections = {
+                lualine_x = {
+                    function()
+                        return vim.treesitter.highlighter.active[vim.api.nvim_get_current_buf()] and "TS" or ""
+                    end,
+                    "encoding", "filetype",
+                },
+            },
         })
     end
 }
